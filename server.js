fee88773df13685238a56630a2d41de7101f7d2f
Andres Long <andreslong95@gmail.com>
2021-09-14 17:12:57 -0500

    gunjs basic server using express

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const Gun = require('gun');
 
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8000
 
 
 //middleware
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-app.use(express.static('public'))
+app.use(Gun.serve).use(express.static('public'));
+
+// Standard Express Middleware
+//----------------------------
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json())
+// app.use(express.static('public'))
 
 //routing
 
 //running
-app.listen(PORT, ()=>console.log('swag on port 3001'))
+const server = app.listen(PORT);
+Gun({file: 'data.json', web: server})
