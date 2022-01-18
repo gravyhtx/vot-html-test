9e91877f709760c66be446ab87bbff5d5f2ffd33
Andres Long <andreslong95@gmail.com>
2022-01-18 15:59:51 -0600

    mongodb working

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const session = require("express-session");
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
