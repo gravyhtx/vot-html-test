2f1933c985b4dc5b5fb63826fcd1ea82530d8981
Andres Long <andreslong95@gmail.com>
2022-01-19 15:02:48 -0600

    spelling error

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 if(process.env.NODE_ENV === "production") {
-  app.use(expres.static("client/build"));
+  app.use(express.static("client/build"));
 }
 app.use(
   session ({
